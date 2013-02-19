a9e42e2f93af300caf408ad9b25712431a2f79fe
Michael Burge <michael.burge@rentrak.com>
2013-02-19 03:19:58 -0800

    Empty set can be extended to a maximal set

--- Matroids.v
+++ Matroids.v
@@ -2,6 +2,7 @@ Require Export Coq.Logic.Classical.
 Require Export Coq.Sets.Constructive_sets.
 Require Export Coq.Sets.Ensembles.
 Require Export Coq.Sets.Finite_sets.
+Require Export Coq.Sets.Finite_sets_facts.
 Require Export Coq.Sets.Powerset.
 Require Export Coq.Setoids.Setoid.
 Section Matroids.
@@ -33,12 +34,11 @@ Section Matroids.
     M_I3 : I3 I
   }.
 
-  Definition Circuits : Matroid -> Ensemble (Ensemble U) :=
-    (fun M : Matroid =>
-       (fun S : Ensemble U =>
-          (~ I M S) /\
-          (forall T : Ensemble U,
-             Strict_Included U T S -> I M T))).
+  Definition Circuits (M : Matroid) : Ensemble (Ensemble U) :=
+    (fun S : Ensemble U =>
+       (~ I M S) /\
+       (forall T : Ensemble U,
+          Strict_Included U T S -> I M T)).
 
   (* C1: The empty set is not a circuit *)
   Definition C1 : Ensemble (Ensemble U) -> Prop :=
@@ -103,6 +103,116 @@ Section Matroids.
     destruct H.
     assumption.
   Qed.
+
+  Definition Base (M : Matroid) (S : Ensemble U) :=
+    I M S /\ forall T : Ensemble U, Strict_Included U S T -> ~ I M S.
+  Theorem Bases_Have_Equal_Size (M : Matroid) (A B : Ensemble U) :
+    (Base M A) /\
+    (Base M B) ->
+    forall (m n : nat),
+      (cardinal U A m) <-> (cardinal U A n).
+  Proof.
+    admit.
+  Qed.
+  Definition Maximal (S : Ensemble U) (P : Ensemble U -> Prop) :=
+    forall (V : Ensemble U),
+      Strict_Included U S V -> ~ P V.
+  Definition Maximal_included (P : Ensemble U -> Prop) (E : Ensemble U) (S : Ensemble U) :=
+    P S /\ Included U S E.
+  Lemma Empty_set_can_be_extended_to_a_maximal_set (P : Ensemble U -> Prop) :
+    P (Empty_set U) ->
+    forall E : Ensemble U,
+      Finite U E -> exists T : Ensemble U, Maximal T (Maximal_included P E).
+  Proof.
+(*    assert (E = Empty_set U \/ E <> Empty_set U). apply classic. *)
+    intro.
+    apply Generalized_induction_on_finite_sets.
+    intros.
+(*    set (prop (S : Ensemble U) := P S /\ Included U S E).*)
+    set (prop (T : Ensemble U) := Maximal T (Maximal_included P X)).
+    set (term := exists T : Ensemble U, prop T).
+    assert (term \/ ~ term). apply classic.
+    destruct H2.
+    assumption.
+    unfold term in H2.
+    assert (forall T : Ensemble U, ~ prop T). firstorder. clear H2.
+(*    unfold prop in H3.
+    unfold Maximal in H3.
+    unfold Maximal_included in H3.
+    unfold Maximal in H1.*)
+    unfold prop. unfold Maximal. unfold Maximal_included.
+    assert (prop (Empty_set U)).
+    firstorder.
+    assert (~ prop (Empty_set U)).
+    apply H3.
+    contradiction.
+  Qed.
+  Lemma Sets_Include_A_Maximal_Independent_Set (M : Matroid) (S : Ensemble U) :
+    exists T : Ensemble U,
+      Included U T S /\
+      I M T /\
+  Proof.
+    intros.
+    apply Generalized_induction_on_finite_sets.
+    intros.
+    destruct (H0 (Empty_set (Ensemble U))).
+    firstorder using M_I1.
+    assert (X (Empty_set U)).
+    apply M_I1.
+    Print ex.
+    destruct (H0 X).
+    
+   Qed.
+  Lemma Independent_iff_No_Circuit_Included (M : Matroid) (S : Ensemble U) :
+    (I M S) <-> forall T : Ensemble U,
+                  Included U T S ->
+                    ~ (Circuits M T).
+  Proof.
+    split.
+    (* -> *)
+    intro.
+    unfold Circuits.
+    intros.
+    contradict H.
+    destruct H as [G H].
+    assert (T = S \/ T <> S). apply classic.
+    destruct H1.
+    rewrite H1 in G. assumption.
+    firstorder using M_I2.
+    (* <- *)
+    intro.
+    unfold Circuits in H.
+    set (term :=  ~ (~ I M S /\ (forall T0 : Ensemble U, Strict_Included U T0 S -> I M T0))).
+    assert term. unfold term. apply (H S). apply Sets_Included_In_Themselves.
+    unfold term in H0.
+    apply not_and_or in H0.
+    destruct H0.
+    apply NNPP in H0. assumption.
+    apply not_all_ex_not in H0.
+    destruct H0.
+    assert (Strict_Included U x S). apply not_imply_elim with (I M x). assumption.
+    assert (~ I M x). apply not_imply_elim2 with (Strict_Included U x S). assumption.
+    assert term. unfold term. apply (H S). apply Sets_Included_In_Themselves.
+    unfold term in H3. apply not_and_or in H3. destruct H3.
+    apply NNPP in H3. assumption.
+    
+    contradict H0.
+    intro.
+    destruct (H S). 
+    destruct (H S). 
+    assert 
+    contradict H.
+    
+    apply not_and_or in H.
+    destruct H.
+    apply NNPP; assumption.
+    apply not_all_ex_not in H.
+    destruct H.
+    
+    assert (I M S \/ (forall T : Ensemble U, Strict_Included U T S -> I M T)).
+    left; assumption.
+    apply not_and_or in H0.
+  Qed.
   Lemma Circuits_Satisfy_C3 : forall M : Matroid, C3 (Circuits M).
   Proof.
     unfold C3.
@@ -121,9 +231,11 @@ Section Matroids.
     assert (H: ~ (Circuits M target /\ Included U target target)).
     apply G.
     apply not_and_or in H.
-    assert (Included U target target).
-    split H.
+    destruct H.
+    unfold Circuits in H.
     
+
+    apply Sets_Included_In_Themselves.
     unfold Included.
     
     assert (H: forall C3 : Ensemble U,
