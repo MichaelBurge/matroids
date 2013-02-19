b85f109886adf91ec7a200c5a635b9dd39be5ba2
Michael Burge <michael.burge@rentrak.com>
2013-02-18 20:48:38 -0800

    C1 & C2 are true of circuits

--- Matroids.v
+++ Matroids.v
@@ -1,7 +1,9 @@
+Require Export Coq.Logic.Classical.
+Require Export Coq.Sets.Constructive_sets.
 Require Export Coq.Sets.Ensembles.
 Require Export Coq.Sets.Finite_sets.
 Require Export Coq.Sets.Powerset.
-
+Require Export Coq.Setoids.Setoid.
 Section Matroids.
   Parameter U : Type.
   Parameter E : Ensemble U.      
@@ -9,29 +11,27 @@ Section Matroids.
   Parameter finite : Finite U E.
 
   (* I1: The empty set is independent *)
-  Definition I1 : Ensemble (Ensemble U) -> Prop :=
-    fun I => I (Empty_set U).
+  Definition I1 (I : Ensemble (Ensemble U)) :=
+    I (Empty_set U).
     (* I2: Subsets of independent sets are independent *)
-  Definition I2 : Ensemble (Ensemble U) -> Prop :=
-      (fun I =>
-         forall (A B : (Ensemble U)),
-           I B /\ (Included U A B) -> I A).
+  Definition I2 (I : Ensemble (Ensemble U)) :=
+    forall (A B : (Ensemble U)),
+      I B /\ (Included U A B) -> I A.
   (* I3: If |A| < |B|, A can be extended to a larger independent set by adding an element from B *)
-  Definition I3 : Ensemble (Ensemble U) -> Prop :=
-    (fun I =>
-       forall (A B : (Ensemble U)),
-       forall (m n : nat),
-         (cardinal U A m ) /\
-         (cardinal U B n) /\
-         m < n ->
-           exists x : U, B x /\ I (Add U A x)).
+  Definition I3 (I : Ensemble (Ensemble U)) := 
+    forall (A B : (Ensemble U)),
+    forall (m n : nat),
+      (cardinal U A m ) /\
+      (cardinal U B n) /\
+      m < n ->
+        exists x : U, B x /\ I (Add U A x).
   
   Record Matroid : Type := {
     I : Ensemble (Ensemble U);
     M_I1 : I1 I;
     M_I2 : I2 I;
     M_I3 : I3 I
-}.
+  }.
 
   Definition Circuits : Matroid -> Ensemble (Ensemble U) :=
     (fun M : Matroid =>
@@ -60,9 +60,107 @@ Section Matroids.
          (C B) /\
          (~ Same_set U A B) /\
          (In U (Intersection U A B) e) ->
-         exists O : Ensemble U,
-           Included U O (Setminus U (Union U A B) (Singleton U e))).
+         exists C3 : Ensemble U,
+           (C C3) /\
+           Included U C3 (Setminus U (Union U A B) (Singleton U e))).
+  Lemma Circuits_Satisfy_C1 : forall M : Matroid, C1 (Circuits M).
+  Proof.
+    unfold C1.
+    intros.
+    intro.
+    unfold Circuits in H.
+    destruct H.
+    contradict H.
+    apply M_I1.
+  Qed.
+  Lemma Circuits_Satisfy_C2 : forall M : Matroid, C2 (Circuits M).
+  Proof.
+    unfold C2.
+    intros.
+    destruct H as [A_circ [B_circ A_sub_B]].
+    assert (G: A = B \/ A <> B). apply classic.
+    destruct G.
+    apply (Extension U); assumption.
+    unfold Circuits in A_circ.
+    unfold Circuits in B_circ.
+    unfold Strict_Included in A_circ.
+    unfold Strict_Included in B_circ.
+    destruct A_circ as [A_not_I A_prop_subs_I].
+    destruct B_circ as [B_not_I B_prop_subs_I].
+    assert (G: Included U A B /\ A <> B -> I M A).
+    apply B_prop_subs_I.
+    contradict A_not_I.
+    auto.
+  Qed.
+  Lemma Sets_Included_In_Themselves :
+    forall A : Ensemble U,
+      Included U A A.
+  Proof.
+    intros.
+    assert (A = A). tauto.
+    apply Extension in H.
+    unfold Same_set in H.
+    destruct H.
+    assumption.
+  Qed.
+  Lemma Circuits_Satisfy_C3 : forall M : Matroid, C3 (Circuits M).
+  Proof.
+    unfold C3.
+    intros.
+    (* target should contain a circuit *)
+    set (target := Setminus U (Union U A B) (Singleton U e)).
+    destruct H as [A_circ [B_circ [A_neq_B e_in_intersect]]].
+    apply NNPP.
+    intro.
+    
+    assert (G: forall C3 : Ensemble U,
+                 ~(Circuits M C3 /\
+                   Included U C3 target)).
+    apply not_ex_all_not. apply H. clear H.
+    assert (I M target).
+    assert (H: ~ (Circuits M target /\ Included U target target)).
+    apply G.
+    apply not_and_or in H.
+    assert (Included U target target).
+    split H.
+    
+    unfold Included.
+    
+    assert (H: forall C3 : Ensemble U,
+                 (~ Circuits M C3) \/
+                 (Included U C3 (Setminus U (Union U A B) (Singleton U e)))).
+    intro. apply not_or_and.
+    destruct (G A).
+    split.
+    assumption.
 
+    contradict A_neq_B.
+    
+    let g := (exists (O : Ensemble U),
+               (Circuits M O) /\
+               (Included U O (Setminus U (Union U A B) (Singleton U e))))
+    in assert (G: g \/ ~g). apply classic.
+
+    contradict.
+   Theorem Circuits_Satisfy_C_Axioms : forall M : Matroid,
+    let cs := Circuits M
+    in (C1 cs /\ C2 cs /\ C3 cs).
+
+  Proof.
+    intro.
+    intro.
+    split.
+    (* C2 *)
+    split.
+    unfold C2.
+    intros.
+    destruct H.
+    destruct H0.
+    unfold Same_set.
+    split.
+    assumption.
+    assert 
+  Qed.
   Record Matroid_Circuit : Type := {
     C : Ensemble (Ensemble U);
     M_C1 : C1 C;
